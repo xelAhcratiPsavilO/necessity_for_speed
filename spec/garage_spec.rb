af80f80b683466a4159d209f227a7290c3f39531
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-21 11:21:57 +0100

    Refactor, DRY

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,12 +2,13 @@ require 'garage'
 
 describe Garage do
 
+  car = Car.new
+
   describe '#release_car' do
     it 'raises an error if there are no cars in the garage' do
       expect { subject.release_car }.to raise_error 'No cars available'
     end
     it 'releases a car' do
-      car = Car.new
       subject.park_car(car)
       expect(subject.release_car).to be_working
     end
@@ -15,14 +16,12 @@ describe Garage do
 
   describe '#park_car' do
     it 'parks a car' do
-      car = Car.new
       expect(subject.park_car(car)).to eq car
     end
   end
 
   describe '#car' do
     it 'shows parked cars' do
-      car = Car.new
       subject.park_car(car)
       expect(subject.car).to eq car
     end

a2667441dc0e8eee586671b9cc80a4cc99232b6e
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-21 11:16:44 +0100

    Fixed all affected tests, Green phase

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -7,21 +7,22 @@ describe Garage do
       expect { subject.release_car }.to raise_error 'No cars available'
     end
     it 'releases a car' do
-      car = subject.release_car
-      expect(car).to be_working
+      car = Car.new
+      subject.park_car(car)
+      expect(subject.release_car).to be_working
     end
   end
 
   describe '#park_car' do
     it 'parks a car' do
-      car = subject.release_car
+      car = Car.new
       expect(subject.park_car(car)).to eq car
     end
   end
 
   describe '#car' do
     it 'shows parked cars' do
-      car = subject.release_car
+      car = Car.new
       subject.park_car(car)
       expect(subject.car).to eq car
     end

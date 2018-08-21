286d6874846d349d9adc55c80fe73b1f03f55522
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-21 10:21:53 +0100

    Refactor, create attr_reader

--- lib/garage.rb
+++ lib/garage.rb
@@ -2,16 +2,14 @@ require_relative 'car'
 
 class Garage
 
+  attr_reader :car
+
   def release_car
     Car.new
   end
 
   def park_car(car)
     @car = car
-  end
-
-  def car
-    @car
   end
 
 end

db4ef8f164a9b8a904bdabadb820104462cd542c
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-21 11:08:52 +0100

    Added ability to raise erros when no cars, Red phase

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,20 +2,29 @@ require 'garage'
 
 describe Garage do
 
-  it 'releases a car' do
-    car = subject.release_car
-    expect(car).to be_working
+  describe '#release_car' do
+    it 'raises an error if there are no cars in the garage' do
+      expect { subject.release_car }.to raise_error 'No cars available'
+    end
+    it 'releases a car' do
+      car = subject.release_car
+      expect(car).to be_working
+    end
   end
 
-  it 'parks a car' do
-    car = subject.release_car
-    expect(subject.park_car(car)).to eq car
+  describe '#park_car' do
+    it 'parks a car' do
+      car = subject.release_car
+      expect(subject.park_car(car)).to eq car
+    end
   end
 
-  it 'shows parked cars' do
-    car = subject.release_car
-    subject.park_car(car)
-    expect(subject.car).to eq car
+  describe '#car' do
+    it 'shows parked cars' do
+      car = subject.release_car
+      subject.park_car(car)
+      expect(subject.car).to eq car
+    end
   end
 
 end

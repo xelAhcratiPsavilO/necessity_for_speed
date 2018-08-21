24b556a57bf326b2002b266e8407ff552227ac1c
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-21 16:01:34 +0100

    Added ability to raise an error at full capacity, Green phase

--- lib/garage.rb
+++ lib/garage.rb
@@ -2,16 +2,20 @@ require_relative 'car'
 
 class Garage
 
+  def initialize
+    @cars = []
+  end
+
   attr_reader :car
 
   def park_car(car)
-    fail 'No more cars allowed' if @car
-    @car = car
+    fail 'No more cars allowed' if @cars.count >=20
+    @cars << car
   end
 
   def release_car
-    fail 'No cars available' unless @car
-    Car.new
+    fail 'No cars available' if @cars.empty?
+    @cars.pop
   end
 
 end

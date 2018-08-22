edb7bc619636f88043ffdd1f9de7180596cd2c60
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-22 13:43:57 +0100

    Added ability to report broken bike, Red phase

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -2,6 +2,18 @@ require 'car'
 
 describe Car do
 
-  it { is_expected.to respond_to :working? }
+  describe '#working?' do
+    it 'states whether the car works' do
+      expect(subject.working?).to be_working
+    end
+  end
+
+  describe '#report_broken' do
+    it 'states that the returned car is broken' do
+      subject.report_broken
+      expect(subject.working?).to be_falsey
+    end
+  end
+
 
 end

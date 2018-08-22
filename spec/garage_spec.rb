77d6d14d43e32530549697921673c4171ffc41ee
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-22 16:22:36 +0100

    Isolated tests with doubles

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -2,35 +2,38 @@ require 'garage'
 
 describe Garage do
 
-  car = Car.new
-
   describe '#release_car' do
     it 'raises an error if there are no cars in the garage' do
       expect { subject.release_car }.to raise_error 'No cars available'
     end
     it 'raises an error if there the car is broken' do
+      car = double(:car)
       car.report_broken
       subject.park_car(car)
       expect { subject.release_car }.to raise_error 'The car is broken'
     end
     it 'releases a car' do
-      subject.park_car(Car.new)
-      expect(subject.release_car).to be_a Car
+      car = double(:car)
+      subject.park_car(car)
+      expect(subject.release_car).not_to be_nill
     end
   end
 
   describe '#park_car' do
     it 'raises an error if the garage has full capacity' do
-      subject.capacity.times { subject.park_car(Car.new) }
+      car = double(:car)
+      subject.capacity.times { subject.park_car(car) }
       expect { subject.park_car(car) }.to raise_error 'No more cars allowed'
     end
     it 'parks a car' do
+      car = double(:car)
       expect(subject.park_car(car)).to eq [car]
     end
   end
 
   describe '#car' do
     it 'shows parked cars' do
+      car = double(:car)
       subject.park_car(car)
       expect(subject.cars).to eq [car]
     end

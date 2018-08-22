678b4474ab732d6551666f722af148a2dbffdf81
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-08-22 09:38:57 +0100

    Refactor, SRP

--- lib/garage.rb
+++ lib/garage.rb
@@ -9,13 +9,23 @@ class Garage
   attr_reader :cars
 
   def park_car(car)
-    fail 'No more cars allowed' if @cars.count >=20
+    fail 'No more cars allowed' if full?
     @cars << car
   end
 
   def release_car
     fail 'No cars available' if @cars.empty?
     @cars.pop
+  end
+
+  private
+
+  def full?
+    @cars.count >= 20
+  end
+
+  def empty?
+    @cars.empty?
   end
 
 end
